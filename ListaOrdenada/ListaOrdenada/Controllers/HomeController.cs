97996464818a68d10702a2aec3a967bd93ab3b65
nathaliaspinula <nathaliaspinula@gmail.com>
2019-09-02 00:32:28 -0300

    Projeto lista ordenada

---
+++ ListaOrdenada/ListaOrdenada/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ListaOrdenada.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace ListaOrdenada.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public string TrataLista(string numeros)
+        {
+            var data = Lista(numeros);
+            var result = String.Join(",", data);
+
+            return result;
+        }
+
+        public int[] Lista(string numeros)
+        {
+            int[] numerosDesordenados = numeros.Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+            int[] numerosOrdenados = { };
+
+            try
+            {
+                Main.ordenarNumeros(numerosDesordenados);
+
+                numerosOrdenados = Main.ordenarNumeros(numerosDesordenados);
+
+                Main.mostrarNumeros(numerosOrdenados);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return numerosOrdenados;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ListaOrdenada/ListaOrdenada/Models/Main.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListaOrdenada.Models
+{
+    public class Main
+    {
+        public static int[] ordenarNumeros(int[] numeros)
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                for (int j = i + 1; j < numeros.Length; j++)
+                {
+                    var comp = comparar(numeros[i], numeros[j]);
+
+                    if(comp == 1)
+                    {
+                        int temp = numeros[i];
+                        numeros[i] = numeros[j];
+                        numeros[j] = temp;
+                    }
+                }
+            }
+            return numeros;
+        }
+
+        // Retorna -1 se a < b, +1 se a > b e 0 se a = b
+        public static int comparar(int a, int b)
+        {
+            if (a < b)
+                return -1;
+
+            if (a > b)
+                return 1;
+
+            return 0;
+        }
+
+        public static void mostrarNumeros(int[] numeros)
+        {
+            if (numeros == null)
+                throw new ArgumentNullException("\"numeros\" nao pode ser nulo.");
+
+            Console.WriteLine(string.Join(", ", numeros));
+        }
+    }
+}
